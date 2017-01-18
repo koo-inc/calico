397c185450d5a545058cf4598b8c0114b5c93163
junsuke <junsuke39@gmail.com>
2017-01-18 14:12:34 +0900

    DefaultApiResource @NoAuth除去

--- web/src/main/java/calicosample/api/resource/DefaultApiResource.java
+++ web/src/main/java/calicosample/api/resource/DefaultApiResource.java
@@ -3,8 +3,6 @@ package calicosample.api.resource;
 import javax.ws.rs.Path;
 
 import calicosample.core.api.ApiResource;
-import calicosample.core.auth.NoAuth;
 
 @Path("/")
-@NoAuth
 public class DefaultApiResource extends ApiResource {}
