5549d13cc7199a7b9893bc6d9dc2ff9364ba24d3
角 輔 <tasuku@freemind.co.jp>
2017-12-28 20:37:10 +0900

    例外処理を調整

--- calico-servlet/src/main/java/jp/co/freemind/calico/servlet/renderer/ExceptionRenderer.java
+++ calico-servlet/src/main/java/jp/co/freemind/calico/servlet/renderer/ExceptionRenderer.java
@@ -5,20 +5,23 @@ import static jp.co.freemind.calico.core.endpoint.result.ResultType.*;
 import javax.servlet.ServletConfig;
 import javax.servlet.http.HttpServletResponse;
 
-import jp.co.freemind.calico.core.exception.UnknownEndpointException;
 import jp.co.freemind.calico.core.exception.AuthorizationException;
-import jp.co.freemind.calico.core.exception.ApplicationException;
+import jp.co.freemind.calico.core.exception.InvalidUserDataException;
+import jp.co.freemind.calico.core.exception.UnknownEndpointException;
 import jp.co.freemind.calico.core.exception.ViolationException;
 import jp.co.freemind.calico.core.validation.Violation;
+import jp.co.freemind.calico.core.zone.UnhandledException;
 
 public class ExceptionRenderer implements Renderer<Object> {
   public void render(ServletConfig conf, HttpServletResponse res, Throwable t) {
+    t = UnhandledException.getPeeled(t);
+
     if (t instanceof ViolationException) {
       Violation violation = ((ViolationException) t).getViolation();
 
       if (t instanceof UnknownEndpointException) {
         render(conf, res, notFound(JSON), violation);
-      } else if(t instanceof ApplicationException) {
+      } else if(t instanceof InvalidUserDataException) {
         render(conf, res, conflict(JSON), violation);
       } else if (t instanceof AuthorizationException) {
         render(conf, res, forbidden(JSON), violation);
@@ -27,7 +30,7 @@ public class ExceptionRenderer implements Renderer<Object> {
       }
     }
     else {
-      render(conf, res, error(JSON), t.getMessage());
+      render(conf, res, error(JSON), new Violation("message", t.getMessage()));
     }
   }
 }
