ec6d7312587defbdf360cabd9b65986b12a6b10f
角 輔 <tasuku@freemind.co.jp>
2020-06-04 18:35:18 +0900

    JDBCドライバーのメモリーリーク修正

--- calico-servlet/src/main/java/jp/co/freemind/calico/servlet/CalicoServletConfig.java
+++ calico-servlet/src/main/java/jp/co/freemind/calico/servlet/CalicoServletConfig.java
@@ -1,12 +1,19 @@
 package jp.co.freemind.calico.servlet;
 
+import java.sql.DriverManager;
+import java.util.Collections;
+
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
+
+import org.apache.logging.log4j.Logger;
 
 import jp.co.freemind.calico.core.config.CalicoConfig;
 
 public abstract class CalicoServletConfig extends CalicoConfig implements ServletContextListener {
+  private static final Logger log = org.apache.logging.log4j.LogManager.getLogger(CalicoServletConfig.class);
   public static String INTERCEPTOR_HANDLERS = CalicoServletConfig.class.getCanonicalName() + ".interceptorHandlers";
+
   @Override
   public void contextInitialized(ServletContextEvent e) {
     initApplication();
@@ -14,6 +21,13 @@ public abstract class CalicoServletConfig extends CalicoConfig implements Servle
   }
 
   @Override
-  public void contextDestroyed(ServletContextEvent e) {
+  public void contextDestroyed(ServletContextEvent event) {
+    Collections.list(DriverManager.getDrivers()).forEach(driver -> {
+      try {
+        DriverManager.deregisterDriver(driver);
+      } catch (Exception e) {
+        log.error(e);
+      }
+    });
   }
 }
